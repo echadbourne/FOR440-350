bd80d30b330715262ebf2953eeaee144f7bb8cca
echadbourne <bethchadbourne@gmail.com>
2025-11-10 15:39:33 -0500

    Update to huntscript

--- ThreatHuntScripts/huntScript.sh
+++ ThreatHuntScripts/huntScript.sh
@@ -8,58 +8,104 @@ do
     echo "3. Investigate Network Connections"
     echo "4. List processes"
     echo "5. List Services"
+    echo "6. List Scheduled Jobs"
+    echo "7. Investigate File Permissions"
     echo "0. Exit Program"
 
     read menuInput
+    echo "Number is $menuInput"
     echo ""
-    if [["$menuInput" == "0"]]; then
+    if [[ "$menuInput" == "0" ]]; then
         echo "Exiting Program..."
         break
-    elif [["$menuInput" == "1"]]; then
-        echo "Listing all users:"
+    elif [[ "$menuInput" == "1" ]]; then
+        echo "==Listing all users=="
         grep -v '/nologin\|/false' /etc/passwd | cut -d: -f1
-        echo "Checking for users with UID 0"
+        echo "==Users with UID 0=="
         awk -F: '$3 == 0 {print $1}' /etc/passwd
-        echo "Checking for users with empty passwords"
+        echo "==Users with empty passwords=="
         sudo awk -F: '($2 == "" || $2 == "!") {print $1}' /etc/shadow
-        echo "Remove users with 'sudo userdel -r [username]'"
         echo "Enter a user to show more information. Enter '0' to exit."
         read userMenu
-        while [["$userMenu" != "0"]]:
+        while [[ "$userMenu" != "0" ]]; do
+            echo "==Groups=="
             groups "$userMenu"
+            echo "==More Information=="
             cat /etc/passwd | grep "$userMenu"
+            echo "==Sudo Permissions=="
             sudo -l -U "$userMenu"
+            echo "==Open Processes=="
             ps aux | grep "$userMenu"
-        done
-    elif [["$menuInput" == "2"]]; then
+            echo "Enter a user to show more information. Enter '0' to exit."
+            read userMenu
+            done
+        echo "Remove users with 'sudo userdel -r [username]'"
+    elif [[ "$menuInput" == "2" ]]; then
         echo "Listing all groups:"
         cut -d: -f1 /etc/group
-        echo "Remove groups with 'sudo groupdel [groupname]'"
         echo "Enter a group name to display members. Enter '0' to exit"
         read groupMenu
-        while [["$groupMenu" != "0"]]; 
-            getent "$groupMenu"
-        done
-    elif [["$menuInput" == "3"]]; then
+        while [[ "$groupMenu" != "0" ]]; do
+            echo "==Members of group $groupMenu=="
+            getent group "$groupMenu"
+            echo "Enter a group name to display members. Enter '0' to exit"
+            read groupMenu
+            done
+        echo "Remove groups with 'sudo groupdel [groupname]'"
+    elif [[ "$menuInput" == "3" ]]; then
         echo "Lising all active network connections:"
         sudo ss -tulnp
-    elif [["$menuInput" == "4"]]; then
+    elif [[ "$menuInput" == "4" ]]; then
         echo "Listing all active processes:"
         ps aux
+        echo "Please select what to investigate:"
+        echo "1. Suspicious processes"
+        echo "2. Processes from suspicious locations"
+        echo "3. Processes with no shell"
+        echo "4. Recently started processes"
+        echo "5. Long Running processes"
+        echo "0. Exit"
+        read processMenu
+        while [[ "$processMenu" != "0" ]]; do
+            if [[ "$processMenu" == "1" ]]; then
+                echo "Listing suspicious processes:"
+                ps aux | grep -E '(bash|ncat|perl)'
+                echo ""
+            elif [[ "$processMenu" == "2" ]]; then
+                echo "Listing processes running from suspicious locations:"
+                ps aux | grep -E '(/tmp|/var/tmp|/dev/shm|/opt)'
+                echo ""
+            elif [[ "$processMenu" == "3" ]]; then
+                echo "Listing processes with no shell:"
+                ps aux | awk '$7 == "?"'
+            elif [[ "$processMenu" == "4" ]]; then
+                ps -eo pid,etime,cmd, --sort=start_time | head -20
+            elif [[ "$processMenu" == "5" ]]; then
+                ps -eo pid,etime,cmd | grep bash
+            fi
+            echo "Please select what to investigate:"
+            echo "1. Suspicious processes"
+            echo "2. Processes from suspicious locations"
+            echo "3. Processes with no shell"
+            echo "4. Recently started processes"
+            echo "5. Long Running processes"
+            echo "0. Exit"
+            read processMenu
+            done
         echo ""
-        echo "Listing suspicious processes:"
-        ps aux | grep -E '(bash|ncat|perl)'
-        echo ""
-        echo "Listing processes running from suspicious locations:"
-        ps aux | grep -E '(/tmp|/var/tmp|/dev/shm|/opt)'
-        echo ""
-        echo "Listing processes with no shell:"
-        ps aux | awk '$7 == "?"'
-        echo "Remove the file the processes is running from, then 'kill' the process"
-    elif [["$menuInput" == "5"]]; then
+        echo "For removal: Remove the file the processes is running from, then 'kill' the process"
+    elif [[ "$menuInput" == "5" ]]; then
         echo "Listing all services:"
         systemctl list-unit-files --type=service
-    elif [["$menuInput" == "6"]]; then
+        echo "Enter a service to investigate, enter '0' to exit"
+        read serviceMenu
+        while [[ "$serviceMenu" != "0" ]]; do
+            systemctl status $serviceMenu
+            systemctl cat $serviceMenu
+            echo "Enter a service to investigate, enter '0' to exit"
+            read serviceMenu
+            done
+    elif [[ "$menuInput" == "6" ]]; then
         echo "Listing root's scheduled jobs:"
         sudo crontab -l
         echo ""
@@ -68,22 +114,21 @@ do
             echo "=== Crontab for $user ==="
             sudo crontab -u $user -l 2>/dev/null
         done
-    elif [["$menuInput" == "7"]]; then
+    elif [[ "$menuInput" == "7" ]]; then
         echo "Listing file permissions of the /tmp directory"
         ls -la /tmp
         echo ""
         echo "Listing file permissions of the /opt directory"
         ls -la /opt
-    elif [["$menuInput" == "8"]]; then
-        echo "Listing SUID files:"
+        echo "==Listing SUID files=="
         find / -perm -4000 -type f 2>/dev/null
         echo ""
-        echo "Listing SGID files:"
+        echo "==Listing SGID files=="
         find / -perm -2000 -type f 2>/dev/null
         echo ""
-        echo "Listing both SUID and SGID Files:"
-        find / -perm /6000 -type f 2>/dev/null
+        #echo "Listing both SUID and SGID Files:"
+        #find / -perm /6000 -type f 2>/dev/null
     else
-        echo "Please select a valid option
+        echo "Please select a valid option"
     fi
 done
